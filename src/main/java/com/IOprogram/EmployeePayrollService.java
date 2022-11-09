fa4f97aca5e25dc893d6e66e3e67804ba384e33b
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-09 20:03:37 +0530

    Added: Read and Write The  New File Using Java IO

--- src/main/java/com/IOprogram/EmployeePayrollService.java
+++ src/main/java/com/IOprogram/EmployeePayrollService.java
@@ -12,7 +12,29 @@ import java.util.Scanner;
 
 public class EmployeePayrollService {
 	
-public static void main(String[] args) {
-	System.out.println("Welcome to Java IO Program");
+
+public void readEmployeePayroll() throws IOException {
+	
+	List<String> lines=Files.readAllLines(Paths.get("C:\\Users\\ASHUTOSH NAWALE\\eclipse-workspace\\Java_IO_Class199\\Payroll.txt"));
+           for (String line:lines) {
+        	   System.out.println(line);
+           }
+}
+public void writeEmployeePayrollData() throws IOException {
+	ArrayList<String> data=new ArrayList<String>();
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Ente the id");
+	data.add(sc.next());
+	System.out.println("Ente the Name");
+	data.add(sc.next());
+	System.out.println("Ente the Salary");
+	data.add(sc.next());	
+	Files.write(Paths.get("C:\\Users\\ASHUTOSH NAWALE\\eclipse-workspace\\Java_IO_Class199\\Payroll.txt"),data, StandardOpenOption.CREATE);
+
+}
+public static void main(String[] args) throws IOException {
+	EmployeePayrollService obj=new EmployeePayrollService();
+	obj.writeEmployeePayrollData();
+	obj.readEmployeePayroll();
 }
 }
