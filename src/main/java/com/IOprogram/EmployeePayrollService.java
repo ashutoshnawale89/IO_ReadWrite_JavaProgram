f67216ac1c6c881bbe0b06c1b59a1060537991a1
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-09 20:00:32 +0530

    Added: Print The Welcome Msg

---
+++ src/main/java/com/IOprogram/EmployeePayrollService.java
@@ -0,0 +1,18 @@
+package com.IOprogram;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class EmployeePayrollService {
+	
+public static void main(String[] args) {
+	System.out.println("Welcome to Java IO Program");
+}
+}
