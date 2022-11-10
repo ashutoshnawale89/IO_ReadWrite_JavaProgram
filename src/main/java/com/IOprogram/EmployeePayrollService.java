4a39297f23696db3f59a9b2a676d34f17377f1c6
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-10 06:52:00 +0530

    Refactor: Create Employee Payroll Data

--- src/main/java/com/IOprogram/EmployeePayrollService.java
+++ src/main/java/com/IOprogram/EmployeePayrollService.java
@@ -26,38 +26,16 @@ public class EmployeePayrollService {
 	
 	
 	public void listFilesDirectory() throws IOException, InterruptedException  {
-		// Activate watch Service
-		WatchService watchService = FileSystems.getDefault().newWatchService();
-
-		// Convert location to file path
-		Path path = Paths.get("C:\\terminalcommands");
-
-		// Register watch service on above path ==> Pass events to be watched
-		// (Create/Delete/Modify)
-		path.register(watchService, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
-
-
-		List<String> logs = new ArrayList<String>();
-
-
-		boolean poll = true;
-		while (poll) {
-
-			// Reference / Pointer current file or folder watching
-			WatchKey key = watchService.take();
-
-			// Printing the events (Create / Modify / Delete)
-			for (WatchEvent<?> event : key.pollEvents()) {
-				System.out.println("Event kind : " + event.kind() + " - File : " + event.context());
-
-				logs.add("Event kind : " + event.kind() + " - File : " + event.context());
-			}
-
-			Files.write(Paths.get("C:\\Users\\ASHUTOSH NAWALE\\Pictures\\Test.txt)"), logs, StandardOpenOption.CREATE);
-	
-			// Reseting the reference so that it can be used later
-			poll = key.reset();
-			}}
+		ArrayList<String> writing = new ArrayList<String>();
+			
+			writing.add("Id : 001");
+			writing.add("Name : Ashutosh");
+			writing.add("Salary : 800000");
+//		File Exist => Override                        
+				// Will create new file if not exist                  
+				Files.write(Paths.get("C:\\Users\\ASHUTOSH NAWALE\\eclipse-workspace\\Java_IO_Class199\\Test1.txt"), writing, StandardOpenOption.CREATE);
+	System.out.println("The Writing is done.......");
+	}
 public static void main(String[] args) throws IOException, InterruptedException {
 	EmployeePayrollService obj=new EmployeePayrollService();
 	obj.listFilesDirectory();
